0e7c8ba8a520e54a21ab043af6e6df978f56c01b
lilili001 <2861166132@qq.com>
2018-07-25 10:34:42 +0800

    first commit

---
+++ src/BdTrans.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yixin
+ * Date: 2018/7/25
+ * Time: 9:47
+ */
+
+namespace Miya\Trans;
+
+
+class BdTrans
+{
+    public function translate()
+    {
+        return 'this is translate method';
+    }
+}
