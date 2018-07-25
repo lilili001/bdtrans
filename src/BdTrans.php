5d9f933ee0b4a0512645b90192e948acd9f3485a
lilili001 <2861166132@qq.com>
2018-07-25 11:14:48 +0800

    edit path

---
+++ src/BdTrans.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yixin
+ * Date: 2018/7/25
+ * Time: 9:47
+ */
+
+namespace MiyaYeh\Trans;
+
+class BdTrans
+{
+    public function translate()
+    {
+        return 'this is translate method';
+    }
+}
---
+++ src/Facades/BdTranFacade.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yixin
+ * Date: 2018/7/25
+ * Time: 10:10
+ */
+
+namespace MiyaYeh\Trans\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class BdTranFacade extends Facade
+{
+    protected static function getFacadeAccessor() { return 'bdtrans'; }
+}
